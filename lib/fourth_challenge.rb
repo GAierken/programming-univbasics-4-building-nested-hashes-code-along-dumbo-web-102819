b97aa1c5eae156a5bafe878698091b8c7610b92c
Guligena Aierken <guligena_aierken@163.com>
2019-10-19 22:17:04 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -1,12 +1,12 @@
 def fourth_challenge
-  epic_tragedy = {
+  e epic_tragedy = {
    :montague => {
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
-      :hero_friends => [{name: "Benvolio", age: "17", attitude: "worried"},
-      {name: "Steven", age: "30", attitude: "confused"}
-        #your hashes here!
+      :hero_friends => [
+         {name: "Mercutio", age: "18", attitude: "hot-headed"},
+         {name: "Benvolio", age: "17", attitude: "worried"}
       ]
    },
    :capulet => {
@@ -14,13 +14,11 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-          {name: "Mercutio", age: "18", attitude: "hot-headed"}, 
-                {name: "Nurse", age: "44", attitude: "worried"}
-
-        #your hashes here!
+          {name: "Steven", age: "30", attitude: "confused"},
+          {name: "Nurse", age: "44", attitude: "worried"}
       ]
    }
-}
+  }
 
 
 
